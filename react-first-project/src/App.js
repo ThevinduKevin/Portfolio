0ba20f581e6aeeb39498f7f038527da32a8af6ce
ThevinduKevin <thevindukevin@gmail.com>
2024-05-05 22:13:39 +0530

    NavBar code

--- react-first-project/src/App.js
+++ react-first-project/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import {NavBar} from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return null
-    
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+   
 }
 
 export default App;
